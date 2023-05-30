5aa5b42f55a1c9dce8cbec1c7a4362dd13333082
faleevand <faleevandre@gmail.com>
2023-05-30 13:05:35 +0300

    update1

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,13 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
 
     private final Needle7 needle7;
 
+    // bean по дефолту создается у конструкотора
     public Egg6(Needle7 needle7) {
         this.needle7 = needle7;
     }
